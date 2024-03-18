b7a802dee2598d0ebaf4213ff2e4e162a29ebf86
Mariano De Simone <desimone.mariano@gmail.com>
2024-03-18 09:15:19 +0100

    First Commit

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "TechPepople Blog";
+export const SITE_DESCRIPTION =
+  "Articles, Stories and Tutorials For Tech people";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
