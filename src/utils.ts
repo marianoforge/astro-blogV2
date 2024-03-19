3566c00b3d7779276c10a876b65e0584a5bbe3f5
Mariano De Simone <desimone.mariano@gmail.com>
2024-03-19 11:35:53 +0100

    Final Commit

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { getCollection, type CollectionEntry } from "astro:content";
+
 //Format Date To String
 export function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
@@ -13,3 +15,11 @@ export function capitalize(str: string): string {
   if (typeof str !== "string" || str.length === 0) return str;
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const allBlogArticles: CollectionEntry<"blog">[] = (
+  await getCollection("blog")
+).sort((a: CollectionEntry<"blog">, b: CollectionEntry<"blog">) => {
+  return (
+    ((b.data.pubDate.valueOf() as number) - a.data.pubDate.valueOf()) as number
+  ).valueOf();
+});
